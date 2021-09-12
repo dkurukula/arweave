81a24c5aee85cc41ad3770655a7eb852e5ecdf99
Dimuth Kurukula <dkurukul@uwo.ca>
2021-09-12 18:12:04 -0400

    prep system for arweave node

---
+++ setupsystem.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATADISK=/dev/sdb
+
+grep -qxF '/etc/sysctl.conf' foo.bar || echo  '/etc/sysctl.conf' >> /etc/sysctl.conf
+grep -qxF 'fs.nr_open=9000001' foo.bar || echo 'fs.nr_open=9000001' >> /etc/sysctl.conf
+sysctl -p
+ulimit -n 900000
+
+sudo tune2fs -O large_dir $DATADISK
+
+#enable huge pages
+sysctl -w vm.nr_hugepages=1000
+grep -qxF 'vm.nr_hugepages=1000' foo.bar || 'vm.nr_hugepages=1000' echo >> /etc/sysctl.d/local.conf
+cat /proc/meminfo | grep HugePages
